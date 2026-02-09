d4687ce15f4d247bb168523b4fdc65eff28f2153
Sada <ambdourazak99@gmail.com>
2026-02-09 17:00:45 +0400

    Create quiz-data.js

---
+++ quiz-data.js
@@ -0,0 +1,12 @@
+const quiz = [
+{
+question: "Quel est le rôle principal de la BCE ?",
+options: ["Contrôler l'inflation", "Financer les États", "Fixer les impôts", "Gérer les universités"],
+answer: 0
+},
+{
+question: "En mathématiques, une suite géométrique est définie par ?",
+options: ["Une différence constante", "Un produit constant", "Une somme constante", "Une division constante"],
+answer: 1
+}
+];
