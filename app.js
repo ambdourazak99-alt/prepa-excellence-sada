7c8aa77d94dedad3966a5298c6c8799661702bc4
Sada <ambdourazak99@gmail.com>
2026-02-09 16:58:48 +0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+function toggleMenu() {
+  document.getElementById("menu").classList.toggle("active");
+}
+
+function goHome() {
+  window.location.href = "index.html";
+}
+
+function openLogin() {
+  window.location.href = "login.html";
+}
+
+function openQuiz() {
+  window.location.href = "dashboard.html";
+}
+
+function openDashboard() {
+  window.location.href = "dashboard.html";
+}
